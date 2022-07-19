ba088dd4d5fcab4a82dafe72c0eb8a454e68ae98
Kuwamura <jobtech.kumamura@gmai.com>
2022-07-19 21:34:59 +0900

    Fixed.

--- JavaScript.js
+++ JavaScript.js
@@ -1,14 +1,15 @@
-var count = 0;
-var flg = true;
+let count = 0;
+let flg = true;
 
 while (flg) {
 if (++count > 3) {
-alert ("game over");
+alert ('game over');
 break;
 }
-if (flg = (prompt ("日本の首都は？") !== "東京")) {
-alert ("不正解です！");
+if (flg = (prompt ('日本の首都は？') !== '東京')) {
+alert ('不正解です！');
 continue;
 }
-alert ("正解です！");
+alert ('正解です！');
+break;
 }
